279ea87fd01f859d7d09877d9952c0dfdd9f3b6e
rishabh-mondal <rishabh.mondal19@gmail.com>
2025-04-23 00:08:08 +0530

    data downloading notebooks

---
+++ run_sentinel_download.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set region name
+region_name="ahmedabad_10km_buffer"
+
+# Navigate to the script directory
+cd /home/rishabh.mondal/Brick-Kilns-project/ijcai_2025_kilns/brick_kilns_neurips_2025 || exit
+
+# Create log directory if it doesn't exist
+log_dir="data_downloading_log"
+
+# Run the script using nohup and save logs under the log directory
+nohup python sentinel_tile_bulk_download.py > "${log_dir}/sentinel_tile_download_${region_name}.log" 2>&1 &
